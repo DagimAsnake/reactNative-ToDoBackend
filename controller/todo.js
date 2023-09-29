4159e293c1b6fd58c6fafefcd250e762989d6152
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-29 06:47:31 -1200

    added done controller

--- controller/todo.js
+++ controller/todo.js
@@ -48,11 +48,51 @@ module.exports.CreateToDo = (async function (req, res) {
 
   module.exports.checkToDo = async function (req, res) {
     const { todoId } = req.params;
-    
     try {
       const updatedTodo = await ToDo.findOneAndUpdate(
         { _id: todoId },
         { checked: true },
+        { new: true }
+      );
+  
+      if (!updatedTodo) {
+        return res.status(404).json({ msg: 'ToDo not found' });
+      }
+  
+      return res.status(200).json({ msg: 'Todo checked successfully' });
+    } catch (error) {
+      console.error('Error checking todo:', error);
+      return res.status(500).json({ error: 'Server error' });
+    }
+  };
+
+  module.exports.getDoneToDo = async function (req, res) {
+    try {
+        const todos = await ToDo.find({ checked: true }).sort({ createdAt: -1 });
+      const data = todos.map((todo) => ({
+        _id: todo._id,
+        title: todo.title,
+        description: todo.description,
+        checked: todo.checked
+      }));
+  
+      return res.status(200).json({
+        todos: data,
+      });
+    } catch (error) {
+      console.error('Error retrieving todo:', error);
+      return res.status(500).json({
+        error: 'Server error',
+      });
+    }
+  };
+
+  module.exports.checkDoneToDo = async function (req, res) {
+    const { todoId } = req.params;
+    try {
+      const updatedTodo = await ToDo.findOneAndUpdate(
+        { _id: todoId },
+        { checked: false },
         { new: true }
       );
   
--- route/todo.js
+++ route/todo.js
@@ -4,5 +4,7 @@ const todoController = require("../controller/todo")
 router.post('/add', todoController.CreateToDo)
 router.get('/', todoController.getToDo)
 router.put('/check/:todoId', todoController.checkToDo)
+router.get('/done', todoController.getDoneToDo)
+router.put('/checkdone/:todoId', todoController.checkDoneToDo)
 
 module.exports = router;
